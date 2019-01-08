2052c1d76ee260e8367374b69084873178264da5
Unknown <david.roupsard@gmail.com>
2019-01-08 13:16:21 +0100

    Ajout image + modif header

--- prototype/header.php
+++ prototype/header.php
@@ -1,34 +1,34 @@
 <header>
-<nav class="sauge">
-<div class="nav-toggle">
-<span class="icon-bar"></span>
-<span class="icon-bar"></span>
-<span class="icon-bar"></span>
-</div>
+  <nav class="sauge">
+    <div class="nav-toggle">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </div>
 
-<ul>
-<li>
-<a href="#">Accueil</a>
-</li>
+    <ul>
+      <li>
+        <a href="#">Accueil</a>
+      </li>
 
-<li>
-<a href="#">Société</a>
-</li>
+      <li>
+        <a href="#">Société</a>
+      </li>
 
-<li>
-<a href="#">Produits</a>
-<ul>
-<li><a href="#">Acacia</a></li>
-<li><a href="#">Pin</a></li>
-<li><a href="#">Châtaigner</a></li>
-<li><a href="#">Bois de chauffe</a></li>
-</ul>
-</li>
+      <li>
+        <a href="#">Produits</a>
+        <ul>
+          <li><a href="#">Acacia</a></li>
+          <li><a href="#">Pin</a></li>
+          <li><a href="#">Châtaigner</a></li>
+          <li><a href="#">Bois de chauffe</a></li>
+        </ul>
+      </li>
 
-<li>
-<a href="#">Contact</a>
-</li>
-</ul>
-</nav>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+    </ul>
+  </nav>
 </header>
 <script src="scripts/header.js"></script>
--- prototype/societe.php
+++ prototype/societe.php
@@ -6,21 +6,14 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <!-- Link CSS -->
-  <link rel="stylesheet" href="styles/global.css">
+  <?php include_once('styles-communs.php'); ?>
   <link rel="stylesheet" href="styles/societe.css">
-  <link rel="stylesheet" href="styles/section-contact.css">
-  <link rel="stylesheet" href="styles/footer.css">
-  <link rel="stylesheet" href="styles/header.css">
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
   <title>B3D | Société</title>
 </head>
 
 <body>
   <!-- INCLUDE DU HEADER -->
-  <header>
-    <?php include('header.php'); ?>
-  </header>
+  <?php include('header.php'); ?>
   <!-- BLOC SPECIFIQUE A LA PAGE -->
   <section class="coeur">
     <div id="encart1" class="encart">
@@ -45,12 +38,8 @@
     <?php include_once('footer.php'); ?>
   </footer>
 
-  <!-- Optional JavaScript -->
-  <script src="scripts/script.js"></script>
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <!-- Script -->
+  <?php include_once('scripts-communs.php'); ?>
 </body>
 
 </html>
