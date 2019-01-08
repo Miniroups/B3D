d9f2a47c226784cf1e685c0c9764bf1e4e01d9a0
jeremie <jeremie.cheney@gmail.com>
2019-01-08 14:04:44 +0100

    ajout jquery bootstrap fontawesome (plus besoin de CDNs)

---
+++ header/header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/fontawsesome-all.min.css" rel="stylesheet" type="text/css">
+<link href="header.css" rel="stylesheet" type="text/css">
+
+<script src="js/jquery-3.2.1.slim.min.js"</script>
+<script src="js/popper.min.js"</script>
+<script src="js/bootstrap.min.js"></script>
+
+<script src="header.js" type="text/javascript"></script>
+
+<title>B3D - Header</title>
+</head>
+<body>
+<header>
+
+<div id="header-logo">
+<img src="img/logo-2.png" alt="B3D logo">
+</div>
+
+<nav>
+<ul class="main-menu">
+  <li class="sub-menu"><a href="societe.html">Société</a>
+    <div class="sub-menu-container">
+      <div class="sub-menu-area arrow-top">
+
+        <div class="product-area-content">
+
+          <a class="product-link" href="#">
+          <img src="img/acacia-5-small.jpg" alt="acacia">
+          <div class="product-name">Acacia</div>
+          </a>
+
+          <a class="product-link" href="#">
+          <img src="img/pin-1-small.jpg" alt="pin">
+          <div class="product-name">Pin</div>
+          </a>
+
+          <a class="product-link" href="#">
+          <img src="img/chataignier-2-small.jpg" alt="chataignier">
+          <div class="product-name">Châtaignier</div>
+          </a>
+
+          <a class="product-link" href="#">
+          <img src="img/acacia-7-small.jpg" alt="bois de chauffage">
+          <div class="product-name">Bois de chauffage</div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </li>
+  <li class="sub-menu"><a href="produits.html">Produits</a>
+    <ul>
+      <li><a href="produits-piquets-acacia.html">Acacia</a>
+      <li><a href="produits-piquets-chataignier.html">Châtaignier</a>
+      <li><a href="produits-piquets-pin.html">Pin</a>
+      <li><a href="produits-bois-chauffage.html">Bois de chauffage</a>
+    </ul>
+  </li>
+</ul>
+</nav>
+
+<div id="header-language-chooser">
+<select>
+  <option value="fr">Français</option>
+  <option value="en">English</option>
+  <option value="es">Español</option>
+  <option value="de">Deutsch</option>
+</select>
+</div>
+
+</header>
+
+</body>
+</html>
--- prototype/styles-communs.php
+++ prototype/styles-communs.php
@@ -1,5 +1,6 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-<link rel="stylesheet" href="styles/global.css">
+<link href="styles/bootstrap.min.css" rel="stylesheet" >
+<link href="styles/fontawsesome-all.min.css" rel="stylesheet" >
+<link href="styles/global.css" rel="stylesheet" >
 <link rel="stylesheet" href="styles/header.css">
 <link rel="stylesheet" href="styles/section-contact.css">
 <link rel="stylesheet" href="styles/footer.css">
