a4a0fd4cb8bac9043927962a59b31aeeabd6e470
jeremie <jeremie.cheney@gmail.com>
2019-01-10 14:24:35 +0100

    suppression antislash bizarre dans section section

--- prototype/section-contact.php
+++ prototype/section-contact.php
@@ -4,7 +4,7 @@
 <p><span>Téléphone :</span> 01 23 45 67 89</p>
 <p><span>Email :</span> julien.procedes@orange.fr</p>
 <p><span>Adresse :</span> " l'Intendant " 47700 Poussignac</p>
-<p><span>Horaire d\'ouverture :</span> Du lundi au vendredi de 8h à 17h00</p>
+<p><span>Horaire d'ouverture :</span> Du lundi au vendredi de 8h à 17h00</p>
 </div>
 <div>
 <img src="images/usine-vue-d-ensemble.jpg" alt="usine vue d\'ensemble">
