f3e2f6591b2d2d7a71fb6a9fff622c479f64de49
JBA19 <44672432+JBA19@users.noreply.github.com>
2019-01-10 10:21:39 +0100

    1st commit page type produits

---
+++ prototype/produits.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Produits</title>
+    <link rel="stylesheet" type="text/css" href="styles/produits.css">
+    <link rel="stylesheet" type="text/css" href="styles/header.css">
+    <link rel="stylesheet" type="text/css" href="styles/global.css">
+    <link rel="stylesheet" type="text/css" href="styles/section-contact.css">
+    <link rel="stylesheet" type="text/css" href="styles/footer.css">
+    <link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css">
+  </head>
+
+    <?php include('header.php'); ?>
+
+  <body>
+
+        <section id="general">
+
+          <h3>Qualité</h3>
+
+          <p>Hac ita persuasione reducti intra moenia bellatores obseratis undique portarum aditibus, propugnaculis insistebant et pinnis, congesta undique saxa telaque habentes in promptu, ut si quis se proripuisset interius, multitudine missilium sterneretur et lapidum.</p>
+
+          <h3>Durabilité</h3>
+
+          <p>Hac ita persuasione reducti intra moenia bellatores obseratis undique portarum aditibus, propugnaculis insistebant et pinnis, congesta undique saxa telaque habentes in promptu, ut si quis se proripuisset interius, multitudine missilium sterneretur et lapidum.</p>
+
+          <h3>Ecologie</h3>
+
+          <p>Hac ita persuasione reducti intra moenia bellatores obseratis undique portarum aditibus, propugnaculis insistebant et pinnis, congesta undique saxa telaque habentes in promptu, ut si quis se proripuisset interius, multitudine missilium sterneretur et lapidum.</p>
+
+        </section>
+
+      <section id="photos">
+
+        <div class="bois">
+          <img src="images/Ac1.jpg" alt="Acacia n°1">
+          <img src="images/Ac2.jpg" alt="Acacia n°2">
+        </div>
+
+        <div class="bois">
+          <img src="images/Ac3.jpg" alt="Acacia n°3">
+          <img src="images/Ac4.jpg" alt="Acacia n°4">
+        </div>
+
+      </section>
+
+      <section id="details">
+
+        <div class="infos">
+
+          <h3>Format disponible</h3>
+
+          <p>Ipsam vero urbem Byzantiorum fuisse refertissimam atque ornatissimam signis quis ignorat? Quae illi, exhausti sumptibus bellisque maximis, cum omnis Mithridaticos impetus totumque Pontum armatum affervescentem in Asiam atque erumpentem, ore repulsum et cervicibus interclusum suis sustinerent, tum, inquam, Byzantii et postea signa illa et reliqua urbis ornanemta sanctissime custodita tenuerunt.</p>
+
+        </div>
+
+        <div class="infos">
+
+          <h3>Caractéristiques</h3>
+
+          <p>Ipsam vero urbem Byzantiorum fuisse refertissimam atque ornatissimam signis quis ignorat? Quae illi, exhausti sumptibus bellisque maximis, cum omnis Mithridaticos impetus totumque Pontum armatum affervescentem in Asiam atque erumpentem, ore repulsum et cervicibus interclusum suis sustinerent, tum, inquam, Byzantii et postea signa illa et reliqua urbis ornanemta sanctissime custodita tenuerunt.</p>
+
+        </div>
+
+      </section>
+
+    <div id="contact">
+      <?php include('section-contact.php') ?>
+    </div>
+
+    <?php include('footer.php') ?>
+
+
+    <script type="text/javascript" href="header.js"></script>
+
+  </body>
+</html>
