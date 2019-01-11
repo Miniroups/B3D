8514ba77048f83d94a63afac479217b93ee5f37a
jeremie <jeremie.cheney@gmail.com>
2019-01-11 16:10:39 +0100

    ajout leaflet + osm aux scripts communs

--- prototype/scripts-communs.php
+++ prototype/scripts-communs.php
@@ -1,5 +1,6 @@
 <script src="scripts/jquery-3.2.1.slim.min.js" type="text/javascript"></script>
 <script src="scripts/popper.min.js" type="text/javascript"></script>
 <script src="scripts/bootstrap.min.js" type="text/javascript"></script>
+<script src="https://npmcdn.com/leaflet@1.0.0-rc.2/dist/leaflet.js"></script>
 <script src="scripts/script.js" type="text/javascript"></script>
 <script src="scripts/header.js" type="text/javascript"></script>
