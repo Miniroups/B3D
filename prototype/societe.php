fd1161756524d7bad4fe94c9fdecd9be3496e4c5
JBA19 <44672432+JBA19@users.noreply.github.com>
2019-01-18 10:41:09 +0100

    Commit intégration contenu

--- prototype/societe.php
+++ prototype/societe.php
@@ -1,34 +1,42 @@
-  <!-- INCLUDE DU HEAD + SPECIFICITEES A LA PAGE -->
-  <?php include_once('ressources/head.php'); ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Link CSS -->
+  <?php include_once('styles-communs.php'); ?>
   <link rel="stylesheet" href="styles/societe.css">
   <title>B3D | Société</title>
 </head>
 
 <body>
   <!-- INCLUDE DU HEADER -->
-  <?php include('ressources/header.php'); ?>
+  <?php include('header.php'); ?>
   <!-- BLOC SPECIFIQUE A LA PAGE -->
   <section class="coeur">
-    <div id="encart1" class="encart">
-      <h3>Nos valeurs</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
     <div id="encart2" class="invert-encart">
       <h3>Notre histoire</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Notre société est située à Poussignac dans le Lot-et-Garonne aux portes du massif landais et au plus prés de la ressource forestière. Dans les années 80, mon père y a construit une scierie de pin maritime fermée en 2015. En 2018, après avoir travaillé dans différents organismes et sociétés de la filière bois, j’ai choisi de revenir sur le site familial pour installer une unité de fabrication de piquets bois.</p>
+    </div>
+    <div id="encart1" class="encart">
+      <h3>Nos produits</h3>
+      <p>La présence d’une diversité d’essences de bois locaux a orienté mon choix sur un outil de transformation souple et flexible permettant de produire des piquets acacia, pin et châtaigner de différentes dimensions. Suivant l’essence et le diamètre des bois transformés deux opérations sont réalisées.</p>
     </div>
     <div id="encart3" class="encart">
       <h3>Notre savoir-faire</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Une opération de planage permettant d’apporter un état de surface raboté et lisse aux bois ronds ou une opération de fente permettant de suivre le fil du bois et conserver la résistance mécanique du bois et éviter les déformations de séchage. En mesure de précaution sanitaire une opération d’écorçage est réalisée pour chaque essence de bois. Cette opération permet d’éviter de transporter d’éventuels parasites comme des insectes xylophages sur le site de la clientèle. La satisfaction client est notre objectif premier et un contrôle de la qualité du produit est réalisé à chaque étape de transformation.</p>
     </div>
   </section>
-  
   <!-- INCLUDE DE LA SECTION CONTACT -->
-  <?php include_once('ressources/section-contact.php'); ?>
+  <div class="section-contact">
+    <?php include_once('section-contact.php'); ?>
+  </div>
   <!-- INCLUDE DU FOOTER -->
-  <?php include_once('ressources/footer.php'); ?>
+  <?php include_once('footer.php'); ?>
   <!-- Script -->
-  <?php include_once('ressources/scripts-communs.php'); ?>
+  <?php include_once('scripts-communs.php'); ?>
 </body>
 
 </html>
