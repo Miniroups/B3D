ce9e72a0e6037a23a5fd94ffe0166bd3cca55c92
JBA19 <44672432+JBA19@users.noreply.github.com>
2019-01-18 13:56:01 +0100

    Commit update content

--- prototype/societe.php
+++ prototype/societe.php
@@ -9,17 +9,17 @@
   <?php include('ressources/header.php'); ?>
   <!-- BLOC SPECIFIQUE A LA PAGE -->
   <section class="coeur">
-    <div id="encart2" class="encart">
+    <div id="encart1" class="encart">
       <h3>Notre histoire</h3>
       <p>Notre société est située à Poussignac dans le Lot-et-Garonne aux portes du massif landais et au plus prés de la ressource forestière. Dans les années 80, mon père y a construit une scierie de pin maritime fermée en 2015. En 2018, après avoir travaillé dans différents organismes et sociétés de la filière bois, j’ai choisi de revenir sur le site familial pour installer une unité de fabrication de piquets bois.</p>
     </div>
-    <div id="encart1" class="invert-encart">
-      <h3>Nos produits</h3>
-      <p>La présence d’une diversité d’essences de bois locaux a orienté mon choix sur un outil de transformation souple et flexible permettant de produire des piquets acacia, pin et châtaigner de différentes dimensions. Suivant l’essence et le diamètre des bois transformés deux opérations sont réalisées.</p>
+    <div id="encart2" class="invert-encart">
+      <h3>Notre savoir-faire</h3>
+      <p>La présence d’une diversité d’essences de bois locaux a orienté mon choix sur un outil de transformation souple et flexible permettant de produire des piquets acacia, pin et châtaigner de différentes dimensions. Suivant l’essence et le diamètre des bois transformés deux opérations sont réalisées. Une opération de planage permettant d’apporter un état de surface raboté et lisse aux bois ronds ou une opération de fente permettant de suivre le fil du bois et conserver la résistance mécanique du bois et éviter les déformations de séchage. En mesure de précaution sanitaire une opération d’écorçage est réalisée pour chaque essence de bois. Cette opération permet d’éviter de transporter d’éventuels parasites comme des insectes xylophages sur le site de la clientèle. La satisfaction client est notre objectif premier et un contrôle de la qualité du produit est réalisé à chaque étape de transformation.</p>
     </div>
     <div id="encart3" class="encart">
-      <h3>Notre savoir-faire</h3>
-      <p>Une opération de planage permettant d’apporter un état de surface raboté et lisse aux bois ronds ou une opération de fente permettant de suivre le fil du bois et conserver la résistance mécanique du bois et éviter les déformations de séchage. En mesure de précaution sanitaire une opération d’écorçage est réalisée pour chaque essence de bois. Cette opération permet d’éviter de transporter d’éventuels parasites comme des insectes xylophages sur le site de la clientèle. La satisfaction client est notre objectif premier et un contrôle de la qualité du produit est réalisé à chaque étape de transformation.</p>
+      <h3>Nos produits</h3>
+      <p>esfszdjixfhkzqniqxcjjixkbhkiqpeixjfvnjsjkchsdcjsdlcjssecsdljsdfjsdcjioocjsdocjcjcsiocjwiljcsilcjwiocjio</p>
     </div>
   </section>
 
