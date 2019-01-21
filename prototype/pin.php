a48ff1a6adf7405550b12cde20812d8d79b0f625
Unknown <david.roupsard@gmail.com>
2019-01-21 13:19:38 +0100

    MAJ Contenu tableau

--- prototype/pin.php
+++ prototype/pin.php
@@ -50,15 +50,69 @@
   <section id="details">
 
     <div>
-      <h3>Usages</h3>
-      <p>Ipsam vero urbem Byzantiorum fuisse refertissimam atque ornatissimam signis quis ignorat? Quae illi, exhausti sumptibus bellisque maximis, cum omnis Mithridaticos impetus totumque Pontum armatum affervescentem in Asiam atque erumpentem, ore
-        repulsum et cervicibus interclusum suis sustinerent, tum, inquam, Byzantii et postea signa illa et reliqua urbis ornanemta sanctissime custodita tenuerunt.</p>
+      <h3>Piquets viticoles</h3>
+      <table >
+      	<tbody>
+      		<tr>
+      			<td colspan="2">Longueurs</td>
+      			<td>2m</td>
+      			<td>2m30</td>
+      			<td>2m50</td>
+      		</tr>
+      		<tr>
+      			<td class="verticalCells" rowspan="3">Diamètres fin bout</td>
+      			<td>7/10cm</td>
+      			<td colspan="3">80/palettes</td>
+      		</tr>
+      		<tr>
+      			<td>10/12cm</td>
+      			<td colspan="3">72/palettes</td>
+      		</tr>
+      		<tr>
+      			<td>12/14cm</td>
+      			<td colspan="3">56/palettes</td>
+      		</tr>
+      	</tbody>
+      </table>
     </div>
 
     <div>
-      <h3>Caractéristiques</h3>
-      <p>Ipsam vero urbem Byzantiorum fuisse refertissimam atque ornatissimam signis quis ignorat? Quae illi, exhausti sumptibus bellisque maximis, cum omnis Mithridaticos impetus totumque Pontum armatum affervescentem in Asiam atque erumpentem, ore
-        repulsum et cervicibus interclusum suis sustinerent, tum, inquam, Byzantii et postea signa illa et reliqua urbis ornanemta sanctissime custodita tenuerunt.</p>
+      <h3>Poteaux arboricoles</h3>
+      <table>
+      	<tbody>
+      		<tr>
+      			<td colspan="2">Longueurs</td>
+      			<td>2m50</td>
+      			<td>2m80</td>
+      			<td>3m</td>
+      			<td>3m50</td>
+      			<td>4m</td>
+      			<td>4m50</td>
+      			<td>5m</td>
+      		</tr>
+      		<tr>
+      			<td class="verticalCells" rowspan="4">Diamètres fin bout</td>
+      			<td>7/10cm</td>
+      			<td colspan="3">80/palettes</td>
+      			<td colspan="4">-</td>
+      		</tr>
+      		<tr>
+      			<td>10/12cm</td>
+      			<td colspan="4">72/palettes</td>
+      			<td colspan="3">64/palettes</td>
+      		</tr>
+      		<tr>
+            <td>12/14cm</td>
+      			<td colspan="4">56/palettes</td>
+      			<td colspan="3">48/palettes</td>
+      		</tr>
+      		<tr>
+            <td>14/16cm</td>
+      			<td colspan="4">42/palettes</td>
+      			<td colspan="3">36/palettes</td>
+      		</tr>
+      	</tbody>
+      </table>
     </div>
 
   </section>
