f4efb03a9f81647de55873e586b52e604326296e
Unknown <david.roupsard@gmail.com>
2019-01-21 15:42:59 +0100

    MAJ Plan du site

--- prototype/plan-du-site.php
+++ prototype/plan-du-site.php
@@ -12,14 +12,14 @@
     <h1>Plan du site</h1>
     <ul>
       <li><a href="societe.php">Société</a></li>
-      <li><a href="produits.php">Produits</a>
-        <ul>
-          <li><a href="acacia.php">Acacia</a></li>
-          <li><a href="pin.php">Pin</a></li>
-          <li><a href="chataigner.php">Châtaigner</a</li>
-            <li><a href="bois-de-chauffe.php">Bois de chauffe</a></li>
-          </ul>
+      <hr>
+      <p>Produits</p>
+      <li><a href="acacia.php">Acacia</a></li>
+      <li><a href="pin.php">Pin</a></li>
+      <li><a href="chataigner.php">Châtaigner</a</li>
+      <li><a href="bois-de-chauffe.php">Bois de chauffe</a></li>
       </li>
+      <hr>
       <li><a href="contact.php">Contact</a></li>
       <li><a href="mentions.php">Mentions</a></li>
       <li><a href="plan-du-site.php">Plan du site</a></li>
