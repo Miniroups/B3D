328498ae52d80ffe200dca0acff7875efe77fc3a
JBA19 <44672432+JBA19@users.noreply.github.com>
2019-01-21 14:00:31 +0100

    MAJ contenu Acacia

--- prototype/acacia.php
+++ prototype/acacia.php
@@ -1,13 +1,19 @@
   <!-- INCLUDE DU HEAD + SPECIFICITEES A LA PAGE -->
-  <?php include_once('parts/head.php'); ?>
+  <?php include_once('ressources/head.php'); ?>
   <link rel="stylesheet" href="styles/produits.css">
   <title>B3D | Acacia</title>
 </head>
 
-<?php include('parts/header.php'); ?>
+<?php include('ressources/header.php'); ?>
 
 <body>
 <h1>Acacia</h1>
+<section id="produits">
+  <div>
+    <h3>Usages</h3>
+    <p>L’ensemble de nos produits acacia sont écorcés pour apporter plus d’esthétique et de propreté aux piquets mais surtout pour éviter de transporter des parasites comme des insectes xylophages sur le site de la clientèle. Nos piquets acacia sont fendus ou planés et une pointe carrée d’une surface comprise entre 1 et 1,5 cm² est réalisée gros bout. L’opération de fente permet au bois de conserver sa résistance mécanique et limiter les déformations de séchage. </p>
+  </div>
+</section>  
 <div id="indicateurs" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#indicateurs" data-slide-to="0" class="active"></li>
@@ -50,26 +56,86 @@
   <section id="details">
 
     <div>
-      <h3>Usages</h3>
-      <p>Ipsam vero urbem Byzantiorum fuisse refertissimam atque ornatissimam signis quis ignorat? Quae illi, exhausti sumptibus bellisque maximis, cum omnis Mithridaticos impetus totumque Pontum armatum affervescentem in Asiam atque erumpentem, ore
-        repulsum et cervicibus interclusum suis sustinerent, tum, inquam, Byzantii et postea signa illa et reliqua urbis ornanemta sanctissime custodita tenuerunt.</p>
+      <h3>Piquets fendus</h3>
+      <table>
+        <tr>
+          <th colspan="2">Longueurs</th>
+          <th>1m40</th>
+          <th>1m80</th>
+          <th>2m</th>
+          <th>2m30</th>
+          <th>2m50</th>
+        </tr>
+        <tr>
+          <td rowspan="4">Périmètres bout fin</td>
+          <td>19/23cm</td>
+          <td colspan="3">160 unités par palette</td>
+          <td>-</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>23/26cm</td>
+          <td colspan="4">150 unités par palette</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>26/29cm</td>
+          <td colspan="5">130 unités par palette</td>
+        </tr>
+        <tr>
+          <td>29cm et plus</td>
+          <td>-</td>
+          <td colspan="4">110 unités par palette</td>
+        </tr>
+      </table>
     </div>
-
     <div>
-      <h3>Caractéristiques</h3>
-      <p>Ipsam vero urbem Byzantiorum fuisse refertissimam atque ornatissimam signis quis ignorat? Quae illi, exhausti sumptibus bellisque maximis, cum omnis Mithridaticos impetus totumque Pontum armatum affervescentem in Asiam atque erumpentem, ore
-        repulsum et cervicibus interclusum suis sustinerent, tum, inquam, Byzantii et postea signa illa et reliqua urbis ornanemta sanctissime custodita tenuerunt.</p>
-    </div>
+      <h3>Piquets ronds</h3>
+      <table>
+        <tr>
+          <th colspan="2">Longueurs</th>
+          <th>1m80</th>
+          <th>2m</th>
+          <th>2m30</th>
+          <th>2m50</th>
+          <th>3m</th>
+          <th>4m</th>
+        </tr>
+        <tr>
+          <td rowspan="4">Diamètres bout fin</td>
+          <td>6/8cm</td>
+          <td colspan="3">80 unités par palette</td>
+          <td>-</td>
+          <td>-</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>8/10cm</td>
+          <td colspan="4">71 unités par palette</td>
+          <td>-</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>10/12cm</td>
+          <td colspan="6">56 unités par palette</td>
+        </tr>
+        <tr>
+          <td>12/15cm</td>
+          <td colspan="6">42 unités par palette</td>
+        </tr>
+      </table>
 
+
+    </div>
   </section>
 
 
   <!-- INCLUDE DE LA SECTION CONTACT -->
-  <?php include_once('parts/section-contact.php'); ?>
+  <?php include_once('ressources/section-contact.php'); ?>
   <!-- INCLUDE DU FOOTER -->
-  <?php include_once('parts/footer.php'); ?>
+  <?php include_once('ressources/footer.php'); ?>
   <!-- Script -->
-  <?php include_once('parts/scripts-communs.php'); ?>
+  <?php include_once('ressources/scripts-communs.php'); ?>
 </body>
 
 </html>
