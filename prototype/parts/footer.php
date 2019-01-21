01c08914335509e6eafb9a501db105fb00027b6b
jeremie <jeremie.cheney@gmail.com>
2019-01-21 11:27:18 +0100

    ajout répertoire parts

---
+++ prototype/parts/footer.php
@@ -0,0 +1,5 @@
+<footer class="container-fluid">
+  <p><a href="plan-du-site.php">Plan du site</a></p>
+  <p id="copyright">Copyright (c) 2018 Copyright Holder All Rights Reserved.</p>
+  <p><a href="mentions.php">Mentions légales</a></p>
+</footer>
---
+++ prototype/parts/header.php
@@ -0,0 +1,49 @@
+<header>
+  <nav class="sauge">
+    <a id="logoB3D" href="index.php"><img src="images/logo.png"></a>
+    <div class="nav-toggle">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </div>
+
+    <ul>
+      <li>
+        <a href="index.php">Accueil</a>
+      </li>
+
+      <li>
+        <a href="societe.php">Société</a>
+      </li>
+
+      <li>
+        <a>Produits</a>
+        <ul>
+          <li><a href="acacia.php">Acacia</a></li>
+          <li><a href="pin.php">Pin</a></li>
+          <li><a href="chataigner.php">Châtaigner</a></li>
+          <li><a href="bois-de-chauffe.php">Bois de chauffe</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="contact.php">Contact</a>
+      </li>
+      <li id="notFlags">
+        <a href="#index.php">Langues</a>
+        <ul>
+          <li><a href="index.php">Français</a></li>
+          <li><a href="index.php">English</a></li>
+          <li><a href="index.php">Español</a></li>
+          <li><a href="index.php">Deutsch</a></li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+  <div id="flags">
+    <a href="index.php"><img src="images/FR.svg" alt="French Flag"></a>
+    <a href="index.php"><img src="images/EN.svg" alt="English Flag"></a>
+    <a href="index.php"><img src="images/ES.svg" alt="Español Flag"></a>
+    <a href="index.php"><img src="images/DE.svg" alt="Deutsch Flag"></a>
+  </div>
+</header>
