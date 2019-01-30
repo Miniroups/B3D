05a7e61ae801149066d882eef17bc8b85d7a96b9
jeremie <jeremie.cheney@gmail.com>
2019-01-30 10:43:46 +0100

    suppression fichiers désormais inutiles (scripts-communs et script.js)

--- prototype/parts/head.php
+++ prototype/parts/head.php
@@ -17,4 +17,4 @@
   <script src="scripts/jquery-3.3.1.min.js" type="text/javascript"></script>
   <script src="scripts/popper.min.js" type="text/javascript"></script>
   <script src="scripts/bootstrap.min.js" type="text/javascript"></script>
-  <script src="scripts/script.js" type="text/javascript"></script>
+  
--- prototype/parts/scripts-communs.php
+++
@@ -1,5 +0,0 @@
-<script src="scripts/jquery-3.3.1.min.js" type="text/javascript"></script>
-<script src="scripts/popper.min.js" type="text/javascript"></script>
-<script src="scripts/bootstrap.min.js" type="text/javascript"></script>
-<script src="scripts/script.js" type="text/javascript"></script>
-<script src="scripts/header.js" type="text/javascript"></script>
