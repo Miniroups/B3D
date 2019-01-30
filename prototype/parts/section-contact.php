3b46400099f6f177e87d4782c54024a76b0ce8d8
Unknown <david.roupsard@gmail.com>
2019-01-30 09:44:22 +0100

    Modification coordonnées

--- prototype/parts/section-contact.php
+++ prototype/parts/section-contact.php
@@ -1,10 +1,10 @@
 <section id="section-contact">
     <div id="p-section-contact">
       <h3>Restons en contact</h3>
-      <p><span>Téléphone :</span> 01 23 45 67 89</p>
+      <p><span>Téléphone :</span> 06 74 90 65 96</p>
       <p><span>Email :</span> julien.procedes@orange.fr</p>
       <p><span>Adresse :</span> " l'Intendant " 47700 Poussignac</p>
-      <p><span>Horaire d'ouverture :</span> Du lundi au vendredi de 8h à 17h00</p>
+      <p><span>Horaire d'ouverture :</span> Du lundi au vendredi 8h-12h 14h-18h et samedi 9h-12h</p>
     </div>
 
     <script src="https://npmcdn.com/leaflet@1.0.0-rc.2/dist/leaflet.js"></script>
