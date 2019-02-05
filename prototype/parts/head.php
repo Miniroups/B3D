35c72e2b23af67e06e3a1c66454830884c00a9d9
jeremie <jeremie.cheney@gmail.com>
2019-02-05 16:46:42 +0100

    suppression link fontawesome

--- prototype/parts/head.php
+++ prototype/parts/head.php
@@ -9,7 +9,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <!-- Link CSS -->
   <link href="styles/bootstrap.min.css" rel="stylesheet" >
-  <link href="styles/fontawsesome-all.min.css" rel="stylesheet" >
   <link href="styles/global.css" rel="stylesheet" >
   <link rel="stylesheet" href="styles/header.css">
   <link rel="stylesheet" href="styles/section-contact.css">
