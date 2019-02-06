aaecbe0dcec9501b7854d5a8f01b1a81a19ac6d0
jeremie <jeremie.cheney@gmail.com>
2019-02-06 15:46:46 +0100

    mise à jour lib/form-data.php

--- prototype/lib/form-data.class.php
+++ prototype/lib/form-data.class.php
@@ -5,59 +5,59 @@ class FormData {
   protected $sanitizedData = [], $errorMessages = [];
 
   function __construct($data, $inputDescriptions, $validationFunctions, $errorMessages) {
-    
+
     $inputNames = array_keys($inputDescriptions);
-    
+
     foreach($data as $key => $raw) {
+
+      if(!in_array($key, $inputNames)) {
+
+        $this->errorMessages[$key] = $errorMessages['unexpected'];
+
+        continue;
+      }
 
       $desc = $inputDescriptions[$key];
 
       if(!empty($desc['type']) && $desc['type'] === 'file') { continue; }
-      
+
       $this->sanitizedData[$key] = '';
-      
-      if(!in_array($key, $inputNames)) {
-        
-        $this->errorMessages[$key] = $errorMessages['unexpected'];
-        
+
+      if($raw === '') {
+
+        if(!empty($desc['required'])) {
+
+          $this->errorMessages[$key] = $errorMessages['required'];
+        }
+
         continue;
       }
 
-      if($raw === '') {
-        
-        if(!empty($desc['required'])) { 
-        
-          $this->errorMessages[$key] = $errorMessages['required'];
-        }
-        
-        continue;
-      }
-      
       if(!empty($desc['max-length']) && strlen($raw) > $desc['max-length']) {
-        
+
         $this->errorMessages[$key] = $errorMessages['max-length'];
-          
+
         continue;
       }
-      
+
       if(!empty($desc['specials']) && preg_match('/[&"<>]/', $raw)) {
-      
+
         $this->errorMessages[$key] = $errorMessages['specials'];
-        
+
         continue;
       }
-      
+
       if(!empty($validationFunctions[$key]) && !$validationFunctions[$key]($raw)) {
 
         $this->errorMessages[$key] = $errorMessages[$key];
-        
+
         continue;
       }
-      
+
       $sanitized = htmlspecialchars(trim($raw), ENT_COMPAT, 'utf-8');
 
       if(!empty($desc['type'])) {
-        
+
         switch($desc['type']) {
 
           case('integer'): $cast = (integer) $sanitized; break;
@@ -65,7 +65,7 @@ class FormData {
           case('float'): $cast = (float) $sanitized; break;
 
           case('boolean'): $cast = ($sanitized === 'true'); break;
-        }        
+        }
       }
       else { $cast = (string) $sanitized; }
 
